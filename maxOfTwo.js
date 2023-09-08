08de5e5ba485892b4e777a60b4c2f0604f0e3391
Megan Turley <turleymegan5@gmail.com>
2023-09-07 18:56:54 -0600

    first and final commit

---
+++ maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 17
+let y = 17
+
+// Write your code below this line.
+if(x > y){
+    console.log(x)
+}else if(x < y){
+    console.log(y)
+}else{
+    console.log("The values are identical")
+}
